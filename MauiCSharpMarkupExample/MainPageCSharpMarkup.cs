c3d80a8a873837d54a319ae89b4bffdca8aba39c
Gerald Versluis <gerald@verslu.is>
2022-05-03 11:44:52 +0200

    Add C# Markup

---
+++ MauiCSharpMarkupExample/MainPageCSharpMarkup.cs
@@ -0,0 +1,71 @@
+﻿using CommunityToolkit.Maui.Markup;
+
+namespace MauiCSharpMarkupExample;
+
+public class MainPageCSharpMarkup : ContentPage
+{
+    int count = 0;
+
+    public MainPageCSharpMarkup()
+    {
+        var scrollView = new ScrollView();
+        var verticalStackLayout = new VerticalStackLayout
+        {
+            Spacing = 25,
+            Children =
+            {
+                new Label
+                {
+                    Text = "Hello, World from C# Markup!",
+                }
+                .Font(size: 32)
+                .CenterHorizontal(),
+
+                new Label
+                {
+                    Text = "Welcome to .NET Multi-platform App UI",
+                }
+                .Font(size: 18)
+                .CenterHorizontal(),
+
+                new Label
+                {
+                    Text = "Current count: 0"
+                }
+                .Font(size: 18, bold: true)
+                .CenterHorizontal()
+                .Assign(out Label countLabel),
+
+                new Button
+                {
+                    Text = "Click me"
+                }
+                .Font(bold: true)
+                .CenterHorizontal()
+                .Invoke(b => b.Clicked += (sender, e) =>
+                {
+                    count++;
+                    countLabel.Text = $"Current count: {count}";
+
+                    SemanticScreenReader.Announce(countLabel.Text);
+                }),
+
+                new Image
+                {
+                    Source = new FileImageSource
+                    {
+                        File = "dotnet_bot.png",
+                    },
+                    WidthRequest = 250,
+                    HeightRequest = 310
+                }
+                .CenterHorizontal()
+            }
+        }
+        .Paddings(30, 30, 30, 30);
+
+        scrollView.Content = verticalStackLayout;
+
+        Content = scrollView;
+    }
+}
--- MauiCSharpMarkupExample/MauiProgram.cs
+++ MauiCSharpMarkupExample/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿namespace MauiCSharpMarkupExample;
+﻿using CommunityToolkit.Maui.Markup;
+
+namespace MauiCSharpMarkupExample;
 
 public static class MauiProgram
 {
@@ -7,6 +9,7 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+			.UseMauiCommunityToolkitMarkup()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
